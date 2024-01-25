670dae0b43964c4baa73f33e8ade147e8963aa66
PHILLIP26 <phillip26@discovery.co.za>
2024-01-25 15:20:29 +0200

    Todo function

--- app/src/main/java/com/phillipdev/todo_list/MainActivity.kt
+++ app/src/main/java/com/phillipdev/todo_list/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.phillipdev.todo_list
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,11 +26,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.phillipdev.todo_list.ui.theme.TODO_LISTTheme
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -32,50 +40,76 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    Todo()
+                    val context: Context = applicationContext
+                    //    instance of the file controller
+                    var fileController = FileController()
+//    to do input variable
+                    var text by remember {
+                        mutableStateOf("")
+                    }
+//    to do array
+                    var todoItems by remember {
+                        mutableStateOf(listOf<String>())
+                    }
+//                    read data from file
+                        try {
+                            todoItems = fileController.readData(applicationContext)
+                        }catch (e : IOException){
+                            println(e.message)
+                        }
+
+
+
+
+                    Column(modifier = Modifier.padding(10.dp)) {
+                        Row(modifier= Modifier
+                            .padding(bottom = 10.dp)
+                            .fillMaxWidth()){
+                            OutlinedTextField(
+                                value = text,
+                                onValueChange = {text = it},
+                                label = { Text("Item To Do")},
+                                modifier = Modifier.weight(1f)
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Button(
+                                onClick = {
+//                                    null check, write input to array and to file then rest textField
+                                    if (text.isNotBlank()) {
+                                        todoItems += text
+                                        fileController.writeData(todoItems,context)
+                                        text=""
+                                    }
+                                },
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            ) {
+                                Text("Add")
+                            }
+
+                        }
+                        TodoList(todoList = todoItems)
+                    }
                 }
             }
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Todo(){
-    var text by remember {
-        mutableStateOf("")
-    }
-    Row(verticalAlignment = Alignment.CenterVertically){
-        OutlinedTextField(
-            value = text,
-            onValueChange = {text = it},
-            label = { Text("Item To Do")},
-            modifier = Modifier.padding(all = 10 .dp)
-        )
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.padding(top = 10 .dp)) {
-                Text("Add")
-        }
-
-    }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TODO_LISTTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            Todo()
+fun TodoList(
+    todoList: List<String>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn{
+        items(todoList) { todo ->
+            Text(
+                text = todo,
+                modifier
+                    .padding(15.dp)
+                    .fillMaxWidth()
+            )
+            Divider()
         }
     }
+
 }
